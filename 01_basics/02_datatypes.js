49d4e318f409ac68727ae83090b39094bf011c34
krushnaoza <krushnaoza137@gmail.com>
2024-12-27 07:37:11 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"Use strict"; //treat all js code as a newer vrsion
+
+// number =>  2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standlone value
+// undefined =>
+// symbol => unique
+// object
+
+
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+
