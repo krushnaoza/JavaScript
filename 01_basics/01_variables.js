afa41f198f34d2019be15817d7cadcd51f1a6135
krushnaoza <krushnaoza137@gmail.com>
2024-12-27 07:38:14 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accId = 14453
+let accEmail = "krushna@gmail.com"
+var accPass = "1235"
+accountCity = "ahmedabad"
+let accState
+
+//accId = 2
+
+accEmail = "krushna@mail.com"
+accPass = "12566"
+accountCity = "surat"
+
+console.log(accId);
+
+console.table([accId,accEmail,accPass,accountCity,accState])
+
+
+
